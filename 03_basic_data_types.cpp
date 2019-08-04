ac43058a6236992e46cee801bb9a5cb657b4258d
Sudhakar17 <sudhakarece17@gmail.com>
2019-08-04 19:02:02 +0200

    printing the values of basic datatypes

---
+++ 03_basic_data_types.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int input1;
+    long input2;
+    char input3;
+    float input4;
+    double input5;
+
+    scanf("%d %ld %c %f %lf", &input1, &input2, &input3, &input4, &input5);
+    printf("%d\n%ld\n%c\n%f\n%lf", input1, input2,input3, input4, input5);
+
+    
+    return 0;
+}
+
