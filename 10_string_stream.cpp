01b37e2194cd5ee4ab970d1aa0af1d3819aa2b1e
Sudhakar17 <sudhakarece17@gmail.com>
2019-09-09 21:57:34 +0200

    string stream into vectors

---
+++ 10_string_stream.cpp
@@ -0,0 +1,32 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+// using namespace std;
+
+std::vector<int> parseInts(std::string str) {
+	// Complete this function
+    std::stringstream ss(str);
+    std::vector<int> vectInt;
+    int i;
+
+    while(ss>>i)
+    {
+        vectInt.push_back(i);
+        if (ss.peek() == ',')
+            ss.ignore();
+    }
+
+    return vectInt;
+}
+
+int main() {
+    std::string str;
+    std::cin >> str;
+    std::vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        std::cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
+
